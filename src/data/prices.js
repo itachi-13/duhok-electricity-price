bd7cae474bcd8f3948363b4833517b7999f694bc
itachi-13 <87619819+itachi-13@users.noreply.github.com>
2025-01-02 22:50:58 +0300

    Update prices to correct data

--- src/data/prices.js
+++ src/data/prices.js
@@ -1,97 +1,99 @@
+// Uncommented data are correct, the one commented beside is the resource we could get
+
 export const _2018 = [
-    { month: "01-2018", price: 13000 },
-    { month: "02-2018", price: 12500 },
-    { month: "03-2018", price: 12000 },
-    { month: "04-2018", price: 9000 },
-    { month: "05-2018", price: 8000 },
-    { month: "06-2018", price: 12000 },
-    { month: "07-2018", price: 14000 },
-    { month: "08-2018", price: 14500 },
-    { month: "09-2018", price: 14250 },
-    { month: "10-2018", price: 10000 },
-    { month: "11-2018", price: 12250 },
-    { month: "12-2018", price: 16500 },
+    { month: "01-2018", price: 13000 }, // https://www.facebook.com/share/p/15dkwxtVp6/
+    { month: "02-2018", price: 12000 }, // https://www.facebook.com/dwarozh.net/posts/pfbid0K4o9aTuWvLZYUkGGnf4CfPzmtchmaL3Uut6fEEPkcvgqxbtNftBSQiER3toUoLFsl
+    { month: "03-2018", price: 10000 }, // https://www.facebook.com/share/p/1Am3cafcbi/
+    { month: "04-2018", price: 8500 }, // Estimated
+    { month: "05-2018", price: 9000 }, // Estimated
+    { month: "06-2018", price: 13250 }, // https://www.facebook.com/share/p/17zEtqhS4z/
+    { month: "07-2018", price: 13000 }, // https://www.facebook.com/share/p/15KcvyNN4s/
+    { month: "08-2018", price: 13750 }, // https://www.facebook.com/share/p/15QHtHX7RY/
+    { month: "09-2018", price: 13000 },
+    { month: "10-2018", price: 12000 },
+    { month: "11-2018", price: 12500 }, // https://www.facebook.com/profile/100067667288178/search/?q=2018
+    { month: "12-2018", price: 8500 }, // https://www.facebook.com/Badinan0/posts/pfbid02nJxRueGS52zo2weRDMHByetDR6nj4d7qyK7S64jrn9CuiWPdBvJy6wyp9fT5SDC7l
 ];
 
 export const _2019 = [
-    { month: "01-2019", price: 14500 },
-    { month: "02-2019", price: 15000 },
-    { month: "03-2019", price: 12750 },
-    { month: "04-2019", price: 9500 },
-    { month: "05-2019", price: 8500 },
+    { month: "01-2019", price: 9000 }, // https://www.facebook.com/share/p/1WDrat19GG/
+    { month: "02-2019", price: 9500 },
+    { month: "03-2019", price: 8750 }, // https://www.facebook.com/share/p/14TGgV1LG1/
+    { month: "04-2019", price: 7500 },
+    { month: "05-2019", price: 5000 }, // https://www.facebook.com/share/p/17wgzHW1fx/
     { month: "06-2019", price: 11500 },
-    { month: "07-2019", price: 15500 },
-    { month: "08-2019", price: 16000 },
-    { month: "09-2019", price: 14250 },
-    { month: "10-2019", price: 10250 },
-    { month: "11-2019", price: 13000 },
-    { month: "12-2019", price: 18500 },
+    { month: "07-2019", price: 8750 }, // https://www.facebook.com/Badinan0/posts/381876309193150/
+    { month: "08-2019", price: 8500 },
+    { month: "09-2019", price: 5250 }, // https://www.facebook.com/share/p/1WrVACCQ2T/
+    { month: "10-2019", price: 4500 },
+    { month: "11-2019", price: 9500 },
+    { month: "12-2019", price: 13000 }, // https://www.facebook.com/permalink.php?story_fbid=pfbid031AKFYrLGC9p5rUtmZNro1cfDLFdyW2B8iZTEWPgJS8SjHxeZSBf5cM5Dn3NZrtJtl&id=706849696362478
 ];
 
 export const _2020 = [
-    { month: "01-2020", price: 15000 },
-    { month: "02-2020", price: 16000 },
-    { month: "03-2020", price: 14000 },
-    { month: "04-2020", price: 10000 },
-    { month: "05-2020", price: 9000 },
-    { month: "06-2020", price: 12500 },
-    { month: "07-2020", price: 17000 },
-    { month: "08-2020", price: 18000 },
-    { month: "09-2020", price: 15000 },
-    { month: "10-2020", price: 12000 },
-    { month: "11-2020", price: 13500 },
-    { month: "12-2020", price: 20000 },
+    { month: "01-2020", price: 11250 },
+    { month: "02-2020", price: 10000 }, // Estimated
+    { month: "03-2020", price: 9500 },
+    { month: "04-2020", price: 6500 },
+    { month: "05-2020", price: 7500 },
+    { month: "06-2020", price: 8500 },
+    { month: "07-2020", price: 9250 },
+    { month: "08-2020", price: 5500 },
+    { month: "09-2020", price: 5750 },
+    { month: "10-2020", price: 3750 },
+    { month: "11-2020", price: 6250 },
+    { month: "12-2020", price: 10500 },
 ];
 
 export const _2021 = [
-    { month: "01-2021", price: 15500 },
-    { month: "02-2021", price: 16500 },
-    { month: "03-2021", price: 13500 },
-    { month: "04-2021", price: 9500 },
-    { month: "05-2021", price: 8500 },
-    { month: "06-2021", price: 13000 },
-    { month: "07-2021", price: 17500 },
-    { month: "08-2021", price: 18500 },
-    { month: "09-2021", price: 14750 },
-    { month: "10-2021", price: 12500 },
-    { month: "11-2021", price: 14000 },
-    { month: "12-2021", price: 21000 },
+    { month: "01-2021", price: 10750 },
+    { month: "02-2021", price: 8250 },
+    { month: "03-2021", price: 8500 },
+    { month: "04-2021", price: 4250 },
+    { month: "05-2021", price: 4750 },
+    { month: "06-2021", price: 9500 },
+    { month: "07-2021", price: 13250 },
+    { month: "08-2021", price: 10000 },
+    { month: "09-2021", price: 6000 },
+    { month: "10-2021", price: 6750 },
+    { month: "11-2021", price: 11750 },
+    { month: "12-2021", price: 15500 },
 ];
 
 export const _2022 = [
-    { month: "01-2022", price: 16000 },
+    { month: "01-2022", price: 17500 },
     { month: "02-2022", price: 17000 },
-    { month: "03-2022", price: 14500 },
-    { month: "04-2022", price: 11000 },
-    { month: "05-2022", price: 9500 },
-    { month: "06-2022", price: 13500 },
-    { month: "07-2022", price: 18000 },
-    { month: "08-2022", price: 19000 },
-    { month: "09-2022", price: 15250 },
-    { month: "10-2022", price: 13000 },
-    { month: "11-2022", price: 15000 },
-    { month: "12-2022", price: 22000 },
+    { month: "03-2022", price: 22000 },
+    { month: "04-2022", price: 11250 },
+    { month: "05-2022", price: 11000 },
+    { month: "06-2022", price: 19750 },
+    { month: "07-2022", price: 19250 },
+    { month: "08-2022", price: 18250 },
+    { month: "09-2022", price: 15000 },
+    { month: "10-2022", price: 14000 },
+    { month: "11-2022", price: 17500 },
+    { month: "12-2022", price: 19500 },
 ];
 
 export const _2023 = [
-    { month: "01-2023", price: 16500 },
-    { month: "02-2023", price: 17500 },
-    { month: "03-2023", price: 15000 },
-    { month: "04-2023", price: 11500 },
-    { month: "05-2023", price: 10000 },
-    { month: "06-2023", price: 14000 },
-    { month: "07-2023", price: 18500 },
-    { month: "08-2023", price: 19500 },
-    { month: "09-2023", price: 15750 },
-    { month: "10-2023", price: 13500 },
-    { month: "11-2023", price: 16000 },
-    { month: "12-2023", price: 21500 },
+    { month: "01-2023", price: 24000 },
+    { month: "02-2023", price: 20500 },
+    { month: "03-2023", price: 9000 },
+    { month: "04-2023", price: 6000 },
+    { month: "05-2023", price: 5000 },
+    { month: "06-2023", price: 10750 },
+    { month: "07-2023", price: 12500 },
+    { month: "08-2023", price: 13250 },
+    { month: "09-2023", price: 9000 },
+    { month: "10-2023", price: 9500 },
+    { month: "11-2023", price: 15500 },
+    { month: "12-2023", price: 19000 },
 ];
 
 export const _2024 = [
-    { month: "01-2024", price: 20000 },
+    { month: "01-2024", price: 19750 },
     { month: "02-2024", price: 18250 },
-    { month: "03-2024", price: 15500 },
+    { month: "03-2024", price: 17500 },
     { month: "04-2024", price: 12500 },
     { month: "05-2024", price: 11750 },
     { month: "06-2024", price: 15250 },
